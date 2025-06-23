d00307e8f0adb2c02522a8f86ad1800783151f67
AnARCHIS12 <fakachampionship@gmail.com>
2025-06-23 23:38:59 +0200

    mv

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  requestMicrophone: () => ipcRenderer.invoke('request-microphone'),
-  selectServer: (server) => ipcRenderer.send('server-selected', server)
-});
